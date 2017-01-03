f0e60b929b72e8d54789fcf236362a1fef9f08f8
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-01-03 19:28:54 +0000

    mail testing 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'serene-shelf-5897.herokuapp.com'
+  host = 'serene-shelf-58997.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
